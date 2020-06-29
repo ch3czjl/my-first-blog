3c8f07ef1d10a521e6780d23a0ee094746b62aaf
ch3czjl <598746155@qq.com>
2020-06-29 10:51:21 +0800

    redirect0

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from flask import Flask, render_template, request, redirect
+from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
